5c960eb493cce7271282d1f4a7c445ee87dd37d2
Brian Warren <bdwarren149@gmail.com>
2017-06-14 19:01:46 -0500

    Stying

---
+++ PrsSolution/Scripts/JavaScript.js
@@ -0,0 +1,20 @@
+﻿// Write your Javascript code.
+
+(function () {
+
+    var $sidebarAndWrapper = $("#sidebar, #wrapper");
+    var $icon = $("#sidebarToggle i.fa");
+
+    $("#sidebarToggle").on("click", function () {
+        $sidebarAndWrapper.toggleClass("hide-sidebar");
+        if ($sidebarAndWrapper.hasClass("hide-sidebar")) {
+            $icon.removeClass("fa-angle-left");
+            $icon.addClass("fa-angle-right");
+        }
+        else {
+            $icon.addClass("fa-angle-left");
+            $icon.removeClass("fa-angle-right");
+        }
+    });
+})
+    ();
